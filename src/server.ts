3ddba3d186fa8ab3fd642a0deacb7780839286dc
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2025-05-13 11:03:54 -0300

    ADD - Dotenv and DynamoDB Connection

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,25 @@
-function print(text: string) {
-    console.log("Text: " + text);
-}
+import express from "express";
+import { DynamoDBClient, ListTablesCommand } from "@aws-sdk/client-dynamodb";
 
-print("A Test");
+import "./configs/env.ts";
+
+// DYNAMODB
+const dynamoDBClient = new DynamoDBClient({
+    region: process.env.AWS_DYNAMODB_REGION || "",
+    credentials: {
+        accessKeyId: process.env.AWS_DYNAMODB_ACCESS_KEY || "",
+        secretAccessKey: process.env.AWS_DYNAMODB_SECRET_ACCESS_KEY || "",
+    }
+});
+
+const command = new ListTablesCommand({});
+const response = await dynamoDBClient.send(command);
+
+console.log(response.TableNames?.join("\n"));
+
+// EXPRESS
+const app = express();
+
+app.listen(3000, () => {
+    console.log("App listening on port " + 3000);
+});
