861ec6ca48122a20631927e4b6dadecf535defa3
Gustavo Martins Pereira <gustavo.martins.pereira.main@gmail.com>
2025-05-16 13:49:35 -0300

    ADD - Block Create Establishment Rule in Establishment when Already Exists

--- src/services/establishmentRule/createEstablishmentRuleUsecase.ts
+++ src/services/establishmentRule/createEstablishmentRuleUsecase.ts
@@ -1,7 +1,11 @@
 import { CreateEstablishmentRuleRequestDTO } from "@models/establishmentRule/request/establishmentRequestDTO.ts";
-import { createEstablishmentRule } from "@repositories/establishmentRuleRepository.ts";
+import { createEstablishmentRule, getEstablishmentRuleByEstablishmentId } from "@repositories/establishmentRuleRepository.ts";
+import { BadRequestError } from "@utils/errors/AppError.ts";
 
 async function createEstablishmentRuleUsecase(establishmentRuleData: CreateEstablishmentRuleRequestDTO) {
+    const establishmentRule = await getEstablishmentRuleByEstablishmentId(establishmentRuleData.establishmentId);
+    if(establishmentRule) throw new BadRequestError("Already exists an establishment rule associated with this establishment");
+
     return await createEstablishmentRule(establishmentRuleData);
 }
 
